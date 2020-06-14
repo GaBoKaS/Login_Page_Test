f701bd1f0c8d7cd92255d04cfd60c8000b4a2436
GaBoKaS <airidas.gabe@gmail.com>
2020-06-14 16:04:29 +0300

    Initial commit

---
+++ Login_Page_Test/Program.cs
@@ -0,0 +1,146 @@
+﻿﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Login_Page_Test
+{
+    class Program : Selenium
+    {
+        static string login;
+        static string pass;
+        static string pageUrl;
+        static void Main(string[] args)
+        {
+            ReadData();
+            LoginTest();
+            LanguageTest();
+            LinkTest();
+            ResponsiveTest();
+
+        }
+        public static void ReadData()
+        {
+            string path = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "TestData.txt");
+            string[] allData = File.ReadAllLines(path);
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(path).ToList();
+            pageUrl = lines[0];
+            login = lines[1];
+            pass = lines[2];
+        }
+
+        public static void LoginTest()
+        {
+            string badEmailWarning = "#react-app > div.Loader > div > div > div > div > div.panel-body > div > div";
+            string loggedInElement = "#page-wrapper > div > h1";
+            string loginByPasswordBtn = "#login-methods-heading-user_credentials > strong";
+            StartUp(pageUrl);
+            Enter("#userIdentifier", "warning please", Keys.Enter);
+            AssertText(badEmailWarning, "The specified user could not be found");
+
+            Enter("#userIdentifier", login, Keys.Enter);
+            ClickAuntilB(loginByPasswordBtn, "#password");
+
+            Enter("#password", pass, Keys.Enter);
+            AssertText(loggedInElement, "Account Overview");
+            Console.WriteLine("Login test is completed");
+            EndTest();
+        }
+        public static void LanguageTest()
+        {
+            string loginTextElement = "#react-app > div.Loader > div > div > div > div > div.panel-heading.panel-heading-narrow > div";
+            string AllLanguagesButton = "#react-language-list-container > ul > li:nth-child(6) > span > i";
+            string LanguageCssBeginning = "body > div:nth-child(15) > div.fade.in.modal > div > div > div.modal-body > ul > li:nth-child(";
+            string[] MultiLanguageText = new string[10]
+            {"LOG IN","PRISIJUNKITE","ВОЙТИ В СИСТЕМУ","PIESLĒGTIES","ZALOGUJ SIĘ","ВХОД","INICIA","LOGI SISSE","MELDEN SIE SICH AN","CONECTARE" };
+            string[] LanguageToTest = new string[10]
+            {"English","LietuviŲ","Русский","Latviešu","Polski","Български","Español","Eesti","Deutsch","Română"};
+            StartUp(pageUrl);
+            AssertText(loginTextElement, MultiLanguageText[0]);
+            for (int i = 1; i <= 9; i++)
+            {
+                Click(AllLanguagesButton);
+                for (int u = 1; u <= 9; u++)
+                {
+                    if (element(LanguageCssBeginning + u + ") > a").Text.Equals(LanguageToTest[i], StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Click(LanguageCssBeginning + u + ") > a");
+                        WaitForTextChange(loginTextElement, MultiLanguageText[i]);
+                        AssertText(loginTextElement, MultiLanguageText[i]);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Language test is completed");
+            EndTest();
+        }
+        public static void LinkTest()
+        {
+            StartUp(pageUrl);
+            string[] elementsToTest = new string[4] {
+                "body > footer > div.footer-info-holder > div:nth-child(3) > ul > li:nth-child(1) > a",
+                "body > footer > div.footer-info-holder > div:nth-child(3) > ul > li:nth-child(2) > a",
+                "body > footer > div.footer-info-holder > div:nth-child(3) > ul > li:nth-child(3) > a",
+                "#react-app > div.text-center > span.text-capitalize > a"
+            };
+            string[] elementForConfirmation = new string[4] {
+                "body > main > article > section:nth-child(1) > div > h1",
+                "body > main > article > section:nth-child(1) > div > h1",
+                "body > main > article > section:nth-child(2) > div > div > h1",
+                "#registration-h1"
+            };
+            string[] desiredText = new string[4]{
+                "Privacy Policy",
+                "Services Agreement",
+                "How to use",
+                "ACCOUNT REGISTRATION"
+            };
+            for (int i = 0; i < elementsToTest.Length; i++)
+            {
+                Click(elementsToTest[i]);
+                AssertText(elementForConfirmation[i], desiredText[i]);
+                PageBack();
+            }
+            LinkTest_NewWindowLinks();
+            Console.WriteLine("Link test is completed");
+            EndTest();
+        }
+        public static void LinkTest_NewWindowLinks()
+        {
+            string[] elementsToTest = new string[2] {
+                "body > footer > div.footer-info-holder > div:nth-child(2) > p > a",
+                "body > footer > div.kayako-wrapper > p > a > span.img-container > img"
+            };
+            string[] elementForConfirmation = new string[2] {
+                "#content > div > div:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(2) > p:nth-child(1) > strong",
+                "#chatform > table:nth-child(3) > tbody > tr:nth-child(1) > td.fieldtitle"
+            };
+            string[] desiredText = new string[2]{
+                "Authorization code",
+                "Choose a question"
+            };
+            for (int i = 0; i < elementsToTest.Length; i++)
+            {
+                Click(elementsToTest[i]);
+                SwitchWindow("New");
+                AssertText(elementForConfirmation[i], desiredText[i]);
+                TabClose();
+                SwitchWindow("Main");
+            }
+        }
+        public static void ResponsiveTest()
+        {
+            string textElement = "body > aside > div.authentication-sidebar-content > div.login-banner-container > h1";
+            StartUp(pageUrl);
+            ChangeResolution(1366, 768);
+            AssertText(textElement, "for more convenient us");
+            ChangeResolution(400, 800);
+            Debug.Assert(!element(textElement).Displayed);
+            Console.WriteLine("Responsive elements test is completed");
+            EndTest();
+        }
+    }
+}
---
+++ Login_Page_Test/Selenium.cs
@@ -0,0 +1,154 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Login_Page_Test
+{
+    class Selenium
+    {
+        static IWebDriver driver;
+        public static void StartUp(string pagelink)
+        { // Sets timeouts for page loading and load starting page
+            driver = new ChromeDriver();
+            _ = driver.Manage().Timeouts().ImplicitWait;
+            driver.Navigate().GoToUrl(pagelink);
+        }
+        public static void Click(string selector)
+        { // Clicks the element specified by selector
+            try
+            {
+                element(selector).Click();
+            }
+            catch (Exception e)
+            {
+                WaitFor(selector);
+                Click(selector);
+            }
+        }
+        public static void ClickAuntilB(string selector1, string selector2)
+        { // keeps clicking element A until element B apears
+            WaitFor(selector1);
+            while (!(element(selector2).Displayed && element(selector2).Enabled))
+            {
+                try
+                {
+                    element(selector1).Click();
+                }
+                catch (Exception e) { }
+            }
+        }
+        public static void Enter(string selector, string text, string actionAfter)
+        { // Types text in a field specified by selector and does desired key press afterwards
+            IWebElement elem = element(selector);
+            WaitFor(selector);
+            while (!element(selector).GetAttribute("value").Equals(text))
+            {
+                elem.Clear();
+                elem.SendKeys(text);
+                WaitForText(selector, text);
+            }
+            elem.SendKeys(actionAfter);
+        }
+        public static void AssertText(string selector, string text)
+        { // checks if element has desired string in it
+            WaitForTextElement(selector);
+            bool EverythingWentGood = driver.FindElement(By.CssSelector(selector)).Text.Contains(text);
+            Debug.Assert(EverythingWentGood);
+        }
+        public static void WaitForText(string selector, string text)
+        { //Waits until certain string is typed into element
+            int i = 0;
+            while (i < 100 && !element(selector).GetAttribute("value").Contains(text))
+            { //when i hits 500 it is a timeout
+                i++;
+            }
+        }
+        public static void WaitForTextChange(string selector, string Text)
+        { //waits until target element has changed its text
+            int i = 0;
+            while (i < 300 && !element(selector).Text.Contains(Text))
+            { //when i hits 500 it is a timeout
+                i++;
+            }
+        }
+        public static void WaitFor(string selector)
+        { // Waits until element is displayed 
+            int i = 0;
+            bool IsVisible = false;
+            while (i < 500 && !IsVisible)
+            { //when i hits 500 it is a timeout
+                try
+                {
+                    i++;
+                    if ((element(selector).Displayed && element(selector).Enabled))
+                    { // Element is visible now, while loop ends
+                        IsVisible = true;
+                    }
+                }
+                catch (Exception e) { }
+            }
+        }
+
+        public static void WaitForTextElement(string selector)
+        { // Waits until non clickable text element is displayed 
+            int i = 0;
+            bool IsVisible = false;
+            while (i < 500 && !IsVisible)
+            { //when i hits 500 it is a timeout
+                try
+                {
+                    i++;
+                    if ((driver.FindElement((By.CssSelector(selector))).Displayed))
+                    { // Element is visible now, while loop ends
+                        IsVisible = true;
+                    }
+                }
+                catch (Exception e) { }
+            }
+        }
+
+        public static IWebElement element(string selector)
+        { // finds and returns element by its Css Selector
+            try
+            {
+                return driver.FindElement(By.CssSelector(selector));
+            }
+            catch (Exception e)
+            {
+                WaitFor(selector);
+                return element(selector);
+            }
+        }
+        public static void EndTest()
+        { // closes browser
+            driver.Quit();
+        }
+
+        public static void PageBack()
+        { // goes page back
+            driver.Navigate().Back();
+        }
+        public static void TabClose()
+        {
+            driver.Close();
+        }
+        // Methods for navigation thought out pages/tabs ---------------------------------
+        public static void SwitchWindow(string page)
+        {
+            if (page.Equals("Main"))
+            {
+                driver.SwitchTo().Window(driver.WindowHandles[0]);
+            }
+            else if (page.Equals("New"))
+            {
+                driver.SwitchTo().Window(driver.WindowHandles[1]);
+            }
+        }
+        public static void ChangeResolution(int width, int height)
+        {
+            driver.Manage().Window.Size = new Size(width, height);
+        }
+    }
+}
